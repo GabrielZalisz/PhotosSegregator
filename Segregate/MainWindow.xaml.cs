3a69e38a6aed70a4fc7786ad2a2f0931e286e9a9
Gabriel Zalisz <gabriel.zalisz@trz.cz>
2018-07-03 09:17:37 +0200

    optimalizace, dole náhled klikací

--- Segregate/MainWindow.xaml.cs
+++ Segregate/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace Segregate
         }
 
         List<string> list_souboru = new List<string>();
-        List<string> bin = new List<string>();
+        //List<string> bin = new List<string>();
         bool povoleno = false;
 
         private void button1_Click(object sender, RoutedEventArgs e)
@@ -62,7 +62,7 @@ namespace Segregate
             }
 
             list_souboru.Clear();
-            bin.Clear();
+            //bin.Clear();
             list_vyber = null;
             lbl00.Background = new SolidColorBrush(Colors.WhiteSmoke);
             lbl0.Background = new SolidColorBrush(Colors.WhiteSmoke);
@@ -107,11 +107,18 @@ namespace Segregate
                 return;
 
             PrehrajObrazek();
+
+            sp.Children.Clear();
+            foreach (string c in list_vyber)
+            {
+                HodDoVyberu(System.IO.Path.Combine(c));
+            }
+            GC.Collect();
         }
 
         void Rotate(Image i, string soubor)
         {
-            ExifReader.ExifReader r = new ExifReader.ExifReader(list_souboru[0]);
+            ExifReader.ExifReader r = new ExifReader.ExifReader(list_souboru[pointer]);
             string orientace = "1";
             foreach (ExifProperty item in r.GetExifProperties())
             {
@@ -137,6 +144,7 @@ namespace Segregate
         }
 
         string[] list_vyber = new string[1];
+        int pointer = 0;
 
         void PrehrajObrazek()
         {
@@ -145,15 +153,16 @@ namespace Segregate
             {
                 BitmapImage a = new BitmapImage();
                 a.BeginInit();
-                a.UriSource = new Uri(bin[bin.Count - 2]);
+                a.UriSource = new Uri(list_souboru[pointer - 2]);
+                a.CacheOption = BitmapCacheOption.OnLoad; 
                 a.EndInit();
                 image00.Source = a;
-                if (list_vyber.Contains(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(bin[bin.Count - 2]))))
+                if (list_vyber.Contains(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[pointer - 2]))))
                     lbl00.Background = new SolidColorBrush(Colors.Green);
                 else
                     lbl00.Background = new SolidColorBrush(Colors.WhiteSmoke);
 
-                Rotate(image00, bin[bin.Count - 2]);
+                Rotate(image00, list_souboru[pointer - 2]);
             }
             catch
             {
@@ -165,15 +174,16 @@ namespace Segregate
             {
                 BitmapImage aa = new BitmapImage();
                 aa.BeginInit();
-                aa.UriSource = new Uri(bin[bin.Count - 1]);
+                aa.UriSource = new Uri(list_souboru[pointer - 1]);
+                aa.CacheOption = BitmapCacheOption.OnLoad;
                 aa.EndInit();
                 image0.Source = aa;
-                if (list_vyber.Contains(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(bin[bin.Count - 1]))))
+                if (list_vyber.Contains(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[pointer - 1]))))
                     lbl0.Background = new SolidColorBrush(Colors.Green);
                 else
                     lbl0.Background = new SolidColorBrush(Colors.WhiteSmoke);
 
-                Rotate(image0, bin[bin.Count - 1]);
+                Rotate(image0, list_souboru[pointer - 1]);
             }
             catch
             {
@@ -185,15 +195,16 @@ namespace Segregate
             {
                 BitmapImage b = new BitmapImage();
                 b.BeginInit();
-                b.UriSource = new Uri(list_souboru[0]);
+                b.UriSource = new Uri(list_souboru[pointer]);
+                b.CacheOption = BitmapCacheOption.OnLoad;
                 b.EndInit();
                 image1.Source = b;
-                if (list_vyber.Contains(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[0]))))
+                if (list_vyber.Contains(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[pointer]))))
                     lbl1.Background = new SolidColorBrush(Colors.Green);
                 else
                     lbl1.Background = new SolidColorBrush(Colors.WhiteSmoke);
 
-                Rotate(image1, list_souboru[0]);
+                Rotate(image1, list_souboru[pointer]);
             }
             catch (Exception eee)
             {
@@ -205,15 +216,16 @@ namespace Segregate
             {
                 BitmapImage bb = new BitmapImage();
                 bb.BeginInit();
-                bb.UriSource = new Uri(list_souboru[1]);
+                bb.UriSource = new Uri(list_souboru[pointer + 1]);
+                bb.CacheOption = BitmapCacheOption.OnLoad;
                 bb.EndInit();
                 image2.Source = bb;
-                if (list_vyber.Contains(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[1]))))
+                if (list_vyber.Contains(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[pointer + 1]))))
                     lbl2.Background = new SolidColorBrush(Colors.Green);
                 else
                     lbl2.Background = new SolidColorBrush(Colors.WhiteSmoke);
 
-                Rotate(image2, list_souboru[1]);
+                Rotate(image2, list_souboru[pointer + 1]);
             }
             catch
             {
@@ -225,15 +237,16 @@ namespace Segregate
             {
                 BitmapImage bbb = new BitmapImage();
                 bbb.BeginInit();
-                bbb.UriSource = new Uri(list_souboru[2]);
+                bbb.UriSource = new Uri(list_souboru[pointer + 2]);
+                bbb.CacheOption = BitmapCacheOption.OnLoad;
                 bbb.EndInit();
                 image3.Source = bbb;
-                if (list_vyber.Contains(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[2]))))
+                if (list_vyber.Contains(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[pointer + 2]))))
                     lbl3.Background = new SolidColorBrush(Colors.Green);
                 else
                     lbl3.Background = new SolidColorBrush(Colors.WhiteSmoke);
 
-                Rotate(image3, list_souboru[2]);
+                Rotate(image3, list_souboru[pointer + 2]);
             }
             catch
             {
@@ -242,11 +255,12 @@ namespace Segregate
                 lbl3.Background = new SolidColorBrush(Colors.WhiteSmoke);
             }
 
-            tbl1.Text = (bin.Count + 1).ToString();
-            tbl2.Text = (list_souboru.Count + bin.Count).ToString();
+            tbl1.Text = (pointer + 1).ToString();
+            tbl2.Text = list_souboru.Count.ToString();
             tbl3.Text = list_vyber.Count().ToString();
 
             lblPocet.Visibility = System.Windows.Visibility.Visible;
+            lbl1.Focus();
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -255,42 +269,50 @@ namespace Segregate
             {
                 if (e.Key == Key.Right)
                 {
-                    if (list_souboru.Count == 1)
+                    if (pointer == list_souboru.Count - 1)
                     {
-                        MessageBox.Show("Konec.");
+                        //MessageBox.Show("Konec.");
                         return;
                     }
                 }
                 if (e.Key == Key.Left)
                 {
-                    if (bin.Count == 0)
+                    if (pointer == 0)
                     {
-                        MessageBox.Show("Nelze.");
+                        //MessageBox.Show("Nelze.");
                         return;
                     }
                 }
                 if (e.Key == Key.Down)
                 {
-                    File.Copy(list_souboru[0], System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[0])), true);
-                    //bin.Add(list_souboru[0]);
-                    //list_souboru.RemoveAt(0);
+                    string new_path = System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[pointer]));
+                    File.Copy(list_souboru[pointer], new_path, true);
+                    HodDoVyberu(new_path);
                     PrehrajObrazek();
                 }
                 if (e.Key == Key.Right)
                 {
-                    bin.Add(list_souboru[0]);
-                    list_souboru.RemoveAt(0);
+                    //bin.Add(list_souboru[0]);
+                    //list_souboru.RemoveAt(0);
+                    pointer++;
+                    if (pointer > list_souboru.Count - 1)
+                        pointer = list_souboru.Count - 1;
                     PrehrajObrazek();
                 }
                 if (e.Key == Key.Left)
                 {
-                    list_souboru.Insert(0, bin[bin.Count - 1]);
-                    bin.RemoveAt(bin.Count - 1);
+                    //list_souboru.Insert(0, bin[bin.Count - 1]);
+                    //bin.RemoveAt(bin.Count - 1);
+                    pointer--;
+                    if (pointer < 0)
+                        pointer = 0;
                     PrehrajObrazek();
                 }
                 if (e.Key == Key.Up)
                 {
-                    File.Delete(System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[0])));
+                    string new_path = System.IO.Path.Combine(tbTo.Text, System.IO.Path.GetFileName(list_souboru[pointer]));
+                    File.Delete(new_path);
+                    VyhodZVyberu(new_path);
                     PrehrajObrazek();
                 }
                 this.Focus();
@@ -298,6 +320,49 @@ namespace Segregate
             catch
             {
 
+            }
+        }
+
+        private void HodDoVyberu(string cesta)
+        {
+            VyhodZVyberu(cesta);
+
+            BitmapImage b = new BitmapImage();
+            b.BeginInit();
+            b.UriSource = new Uri(cesta);
+            b.CacheOption = BitmapCacheOption.OnLoad;
+            b.EndInit();
+            Image iii = new Image();
+            iii.Source = b;
+            iii.Margin = new Thickness(3, 0, 0, 0);
+
+            Rotate(iii, cesta);
+
+            iii.Tag = cesta;
+            iii.MouseLeftButtonDown += Iii_MouseLeftButtonDown;
+            sp.Children.Add(iii);
+            iii.BringIntoView();
+        }
+
+        private void Iii_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            string tag = (sender as Image).Tag.ToString();
+            string fn = System.IO.Path.GetFileName(tag);
+            string c = System.IO.Path.Combine(tbFrom.Text, fn);
+            int ppp = list_souboru.IndexOf(c);
+            if (ppp >= 0)
+            {
+                pointer = ppp;
+                PrehrajObrazek();
+            }
+        }
+
+        private void VyhodZVyberu(string cesta)
+        {
+            List<Image> e = sp.Children.OfType<Image>().Where(q => q.Tag.ToString().Contains(cesta)).ToList();
+            foreach (Image iii in e)
+            {
+                sp.Children.Remove(iii);
             }
         }
 
@@ -311,7 +376,7 @@ namespace Segregate
             lblPocet.Visibility = System.Windows.Visibility.Collapsed;
             list_souboru.Clear();
             list_vyber = null;
-            bin.Clear();
+            //bin.Clear();
             tbFrom.IsEnabled = true;
             tbTo.IsEnabled = true;
             btnStop.IsEnabled = false;
@@ -359,6 +424,49 @@ namespace Segregate
                 System.IO.File.Move(fotka, System.IO.Path.Combine( System.IO.Path.GetDirectoryName(fotka), name));
 
             }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            row5.Height = new GridLength(100, GridUnitType.Star);
+            colL.Width = new GridLength(150, GridUnitType.Star);
+            colR.Width = new GridLength(150, GridUnitType.Star);
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            row5.Height = new GridLength(0);
+            colL.Width = new GridLength(100, GridUnitType.Star);
+            colR.Width = new GridLength(100, GridUnitType.Star);
+        }
+
+        private void btn10_Click(object sender, RoutedEventArgs e)
+        {
+            Inkrement(10);
+        }
+
+        private void btn20_Click(object sender, RoutedEventArgs e)
+        {
+            Inkrement(20);
+        }
+
+        private void btn50_Click(object sender, RoutedEventArgs e)
+        {
+            Inkrement(50);
+        }
+
+        void Inkrement(int i)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+            {
+                i = i * -1;
+            }
+            pointer += i;
+            if (pointer < 0)
+                pointer = 0;
+            if (pointer > list_souboru.Count - 1)
+                pointer = list_souboru.Count - 1;
+            PrehrajObrazek();
         }
     }
 }
